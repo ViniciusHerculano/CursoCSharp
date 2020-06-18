6e1e2340ca7a02e0fd1b2fa0ed3b8428ea4d82ec
Vinicius Herculano <viniciusherculanomap@gmail.com>
2020-06-18 00:02:13 -0300

    Projeto iniciado, mostrando a msg Ola mundo

---
+++ ModuloGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModuloGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
